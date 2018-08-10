2152ddd0a68e4e2d903488d0aef15638ea98ee37
Lee Boon Kong <lee.boon.kong@hotmail.com>
2018-08-10 15:58:58 +0800

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
 var url = 'mongodb://localhost:27017';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors});
+  res.render('index', { title: 'Form Validation and Mongo Database', 
+                        success: req.session.success, 
+                        errors: req.session.errors,
+                        errorsUpdate: req.session.errorsUpdate});
   req.session.errors = null;
+  req.session.errorsUpdate = null;
   req.session.success = null;
+  req.session.successUpdate = null;
 });
 
 router.get('/get-data', function(req, res, next){
   var resultArray = [];
+
   mongo.connect(url, function(err, client){
     var db = client.db('test');
 
@@ -25,37 +32,87 @@ router.get('/get-data', function(req, res, next){
       resultArray.push(doc);
     }, function(){
       client.close();
-      res.render('index', {items: resultArray});
+      res.render('index', {title: 'Form Validation and Mongo Database',
+                            items: resultArray});
     });
   });
 });
 
 router.post('/insert', function(req, res, next){
-  var item ={
-    email: req.body.email,
-    password: req.body.password
-  };
+  req.check('email', 'Invalid Email Address').isEmail();
+  req.check('password', 'Password is invalid').isLength({min:4}).equals(req.body.confirmPassword);
+  
+  var errors = req.validationErrors();
+  if(!errors){
+    var item ={
+      email: req.body.email,
+      password: req.body.password
+    };
+
+    mongo.connect(url, function(err, client){
+      var db = client.db('test');
+
+      assert.equal(null, err);
+      db.collection('user-data').insertOne(item, function(err, result){
+        assert.equal(null, err);
+        console.log('Item Inserted');
+        client.close();
+      });
+    });
+    req.session.success = true;
+  } else {
+    req.session.errors = errors;
+    req.session.success = false;
+  }
+
+  res.redirect('/');
+});
+
+router.post('/update', function(req, res, next){
+  req.check('email', 'Invalid Email Address').isEmail();
+  req.check('password', 'Invalid Password').isLength({min: 4}).equals(req.body.confirmPassword);
+
+  var errors = req.validationErrors();
+  if(!errors){
+    var item = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    
+    var id = req.body.userid;
+
+    mongo.connect(url, function(err, client){
+      var db = client.db('test');
+
+      assert.equal(null, err);
+      db.collection('user-data').updateOne({'_id': objectId(id)}, {$set: item}, function(err, result){
+        assert.equal(null, err);
+        console.log('Item Updated');
+        client.close();
+      })
+    });
+    req.session.successUpdate = true;
+  } else {
+    req.session.errorsUpdate = errors;
+    req.session.successUpdate = false;
+  }
+  
+  res.redirect('/');
+});
+
+router.post('/delete', function(req, res, next){
+  var id = req.body.userid;
 
   mongo.connect(url, function(err, client){
     var db = client.db('test');
 
     assert.equal(null, err);
-    db.collection('user-data').insertOne(item, function(err, result){
-      assert.equal(null, err);
-      console.log('Item Inserted');
-      client.close();
+    db.collection('user-data').deleteOne({'_id': objectId(id)}, function(err,result){
+      console.log('Item Deleted');
+      db.close
     });
-
-    res.redirect('/');
   });
-});
-
-router.get('/update', function(req, res, next){
-  
-});
-
-router.get('/delete', function(req, res, next){
-  
+  res.redirect('/');
 });
 
 module.exports = router;
